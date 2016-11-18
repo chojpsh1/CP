0062444808664ac112d00cb2d25b6d4e730d80f6
OhJaeSeong <ojskih@naver.com>
2016-11-18 19:58:43 +0900

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -7,7 +7,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class MyWorld extends World
-{
+{   int lob1,lob2;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -17,8 +17,11 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        lob1=Greenfoot.getRandomNumber(600);
+        lob2=Greenfoot.getRandomNumber(600);
         addObject( new crab(), 150, 110 );
         addObject( new turtle(), 50, 200 );
+        addObject( new lobster(), lob1, lob2);
         int antiworm = 0;
         addObject(new worm(), Greenfoot.getRandomNumber(getWidth()), Greenfoot.getRandomNumber(getHeight()));
         addObject(new worm(), Greenfoot.getRandomNumber(getWidth()), Greenfoot.getRandomNumber(getHeight()));
